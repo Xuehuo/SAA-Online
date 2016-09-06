6a47dcc2d94546b9257ef3d8a7e8f1db40ba727e
Tonny <me@lyzde.com>
2016-09-06 09:35:52 +0800

    Move AjaxHandler

---
+++ App_code/SAAO/AjaxHandler.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Web;
+using System.Web.SessionState;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace SAAO
+{
+    /// <summary>
+    /// General Ajax processing handler
+    /// </summary>
+    public class AjaxHandler : IHttpHandler, IRequiresSessionState
+    {
+        protected struct Return
+        {
+            public int Flag;
+            public object Data;
+        }
+
+        protected Return R;
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "application/json";
+            try
+            {
+                Process(context);
+            }
+            catch (Exception ex)
+            {
+                R.Flag = 3;
+                Utility.Log(ex);
+            }
+            if (R.Flag == -1) return;
+            context.Response.Write(JsonConvert.SerializeObject(
+                value: R,
+                formatting: Formatting.Indented,
+                settings: new JsonSerializerSettings { ContractResolver = new CamelCasePropertyNamesContractResolver() })
+            );
+        }
+
+        public virtual void Process(HttpContext context) { }
+
+        public bool IsReusable => false;
+    }
+}
